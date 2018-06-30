a373a7c8e86ed80afba2ebde98e9f89c8919d2ff
Jiajiaju <jjiaju@gmail.com>
2018-07-01 00:18:00 +0800

    load animation config

--- Classes/battle/manager/InfoManager.cpp
+++ Classes/battle/manager/InfoManager.cpp
@@ -23,13 +23,25 @@ void InfoManager::init(){
     animationConfig.Parse<0>(animationString.c_str());
     for (rapidjson::SizeType i = 0; i < animationConfig.Size(); ++i){
         rapidjson::Value& item = animationConfig[i];
-        CCLOG("%s", item["Name"].GetString());
         std::string name = item["Name"].GetString();
         CharacterAnimationInfo info;
         info.name = name;
-        for (rapidjson::SizeType ii = 0; ii < item["Animations"].Size(); ++i){
-//            rapidjson::Value animation = item["Animations"][ii];
-//            info.animationFrame[animation["Name"].GetString()].push_back(animation["StartFrame"].GetInt());
+        for (rapidjson::SizeType ii = 0; ii < item["Animations"].Size(); ++ii){
+            rapidjson::Value& animation = item["Animations"][ii];
+            info.animationFrame[animation["Name"].GetString()].push_back(animation["StartFrame"].GetInt());
+            info.animationFrame[animation["Name"].GetString()].push_back(animation["EndFrame"].GetInt());
+            info.animationFrame[animation["Name"].GetString()].push_back(animation["EventFrame"].GetInt());
+        }
+        characterAnimationInfo[info.name] = info;
+    }
+    
+    for (auto iter = characterAnimationInfo.begin(); iter != characterAnimationInfo.end(); ++iter){
+        CCLOG("\n%s, %s", iter->first.c_str(), iter->second.name.c_str());
+        for (auto iteriter = iter->second.animationFrame.begin(); iteriter != iter->second.animationFrame.end(); ++iteriter){
+            CCLOG("%s ", iteriter->first.c_str());
+            for (auto iteriteriter = iteriter->second.begin(); iteriteriter != iteriter->second.end(); ++iteriteriter){
+                CCLOG("%d, ", *iteriteriter);
+            }
         }
     }
 }
